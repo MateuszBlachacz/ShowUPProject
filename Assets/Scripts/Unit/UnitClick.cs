ce3c016a3f6e23b6bbc5042e6ddd51e552711755
BlachaczMateusz <blachacz.mateusz@gmail.com>
2024-01-24 22:18:50 +0100

    Add Unit check is ciicked

--- Assets/Scripts/Unit/UnitClick.cs
+++ Assets/Scripts/Unit/UnitClick.cs
@@ -1,18 +1,51 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UnitClick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera myCam;
+    
+    public LayerMask clickable;
+    public LayerMask groung;
+
+    private UnitAction input; 
+
+    private void Start()
     {
+        myCam = Camera.main;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Debug.Log("Click");
+            RaycastHit hit;
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+            Debug.Log(ray.ToString());
+            if (Physics.Raycast(ray: ray, hitInfo: out hit, Mathf.Infinity, clickable))
+            {
+                //Debug.Log("Hit clickable");
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    UnitSelections.Instance.ShiftClickSelect(hit.collider.gameObject);
+                }
+                else
+                {
+                    UnitSelections.Instance.ClickSelect(hit.collider.gameObject);
+                }
+
+            } else
+            {
+                if (!Input.GetKey(KeyCode.LeftShift))
+                {
+                    UnitSelections.Instance.DeselectAll();
+                }
+                
+            }
+        }
     }
+
 }
