c02cee17b552441b387ca668edf944f82e1b7697
BlachaczMateusz <blachacz.mateusz@gmail.com>
2024-01-24 22:17:02 +0100

    Add Animation script
    Add animation to select objet

---
+++ Assets/Scripts/Unit/SelectAnimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectAnimation : MonoBehaviour
+{
+    float minsize = 0.9f;
+    float maxsize = 1.2f;
+    float chVal = 0.15f;
+
+    void Update()
+    {
+        Vector3 changescale = new Vector3(changedValue(), 0, chVal);
+        transform.localScale += changescale * Time.deltaTime;
+    }
+
+    float changedValue()
+    {
+        if (transform.localScale.x <= minsize) chVal *= -1;
+        if (transform.localScale.x >= maxsize) chVal *= -1;
+        Debug.Log(chVal);
+        return chVal;
+    }
+}
