447bfdb9882daac7b746fa98fe0d3d403cd1184c
BlachaczMateusz <blachacz.mateusz@gmail.com>
2024-01-24 15:03:42 +0100

    Add skybox manager
    Add animation for skybox
    Create Manager Object and attached script SkyManager to it.

---
+++ Assets/Scripts/SkyManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Use to make simple anmation of skybox
+public class SkyManager : MonoBehaviour
+{
+    public bool bAnimatedSky;
+    [SerializeField]
+    float skySpeed;
+    
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation",Time.deltaTime * skySpeed);
+    }
+}
